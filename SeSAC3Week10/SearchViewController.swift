1ca4ca41abb6b1b230d054fc0ceec4de6c091556
Hyuk0510 <hyouk970510@gmail.com>
2023-09-21 23:15:13 +0900

    9/21 실습

---
+++ SeSAC3Week10/SearchViewController.swift
@@ -0,0 +1,186 @@
+//
+//  SearchViewController.swift
+//  SeSAC3Week10
+//
+//  Created by 선상혁 on 2023/09/21.
+//
+
+import UIKit
+import SnapKit
+
+class SearchViewController: UIViewController {
+    
+    let list = Array(0...100)
+    
+    var collectionView = UICollectionView(frame: .zero, collectionViewLayout: configureCollectionLayout())
+    var dataSource: UICollectionViewDiffableDataSource<Int, Int>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureHierarchy()
+        configureLayout()
+        configureDataSource()
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(collectionView)
+    }
+    
+    func configureLayout() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    static func configureCollectionLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 50, height: 50)
+        layout.scrollDirection = .vertical
+        return layout
+    }
+    
+    func configureDataSource() {
+        
+        let cellRegistration = UICollectionView.CellRegistration<SearchCollectionViewCell, Int> { cell, indexPath, itemIdentifier in
+            cell.imageView.image = UIImage(systemName: "star")
+            cell.label.text = "\(itemIdentifier)번"
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(list)
+        dataSource.apply(snapshot)
+    }
+    
+}
+
+//class SearchViewController: UIViewController {
+//
+//    let scrollView = UIScrollView()
+//    let contentView = UIView()
+//
+//    let imageView = UIImageView()
+//    let label = UILabel()
+//    let button = UIButton()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        configureHierarchy()
+//        configureLayout()
+//        configureContentView()
+//    }
+//
+//    func configureContentView() {
+//        contentView.addSubview(imageView)
+//        contentView.addSubview(label)
+//        contentView.addSubview(button)
+//
+//        imageView.backgroundColor = .orange
+//        button.backgroundColor = .magenta
+//        label.backgroundColor = .systemGreen
+//
+//        imageView.snp.makeConstraints { make in
+//            make.top.horizontalEdges.equalTo(contentView).inset(10)
+//            make.height.equalTo(200)
+//        }
+//
+//        button.snp.makeConstraints { make in
+//            make.bottom.horizontalEdges.equalTo(contentView).inset(10)
+//            make.height.equalTo(80)
+//        }
+//
+//        label.text = "Adfsa\nfs\nda\nsd\nfa\nsf\nds\nfasfadsAdfsa\nfs\nda\nsd\nfa\nsf\nds\nfasfadsAdfsa\nfs\nda\nsd\nfa\nsf\nds\nfasfadsAdfsa\nfs\nda\nsd\nfa\nsf\nds\nfasfadsAdfsa\nfs\nda\nsd\nfa\nsf\nds\nfasfadsAdfsa\nfs\nda\nsd\nfa\nsf\nds\nfasfads"
+//        label.numberOfLines = 0
+//        label.textColor = .white
+//        label.snp.makeConstraints { make in
+//            make.horizontalEdges.equalTo(contentView)
+//            make.top.equalTo(imageView.snp.bottom).offset(50)
+//            make.bottom.equalTo(button.snp.top).offset(-50)
+//        }
+//    }
+//
+//    func configureHierarchy() {
+//        view.addSubview(scrollView)
+//        scrollView.addSubview(contentView)
+//    }
+//
+//    func configureLayout() {
+//        scrollView.bounces = false
+//        scrollView.backgroundColor = .lightGray
+//        scrollView.snp.makeConstraints { make in
+//            make.edges.equalTo(view.safeAreaLayoutGuide)
+//        }
+//
+//        contentView.backgroundColor = .white
+//        contentView.snp.makeConstraints { make in
+//            make.verticalEdges.equalTo(scrollView)
+//            make.width.equalTo(scrollView.snp.width)
+//        }
+//    }
+//}
+
+//class SearchViewController: UIViewController {
+//
+//    let scrollView = UIScrollView()
+//    let stackView = UIStackView()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = .white
+//        configureHierarchy()
+//        configureLayout()
+//    }
+//
+//    func configureHierarchy() {
+//        view.addSubview(scrollView)
+//        scrollView.addSubview(stackView)
+//    }
+//
+//    func configureLayout() {
+//        scrollView.backgroundColor = .lightGray
+//        scrollView.snp.makeConstraints { make in
+//            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+//            make.height.equalTo(70)
+//        }
+//
+//        stackView.spacing = 16
+//        stackView.backgroundColor = .black
+//        stackView.snp.makeConstraints { make in
+//            make.height.equalTo(scrollView)
+//        }
+//        configureStackView()
+//    }
+//
+//    func configureStackView() {
+//        let label1 = UILabel()
+//        label1.text = "안녕하세요"
+//        label1.textColor = .white
+//        label1.backgroundColor = .brown
+//        stackView.addArrangedSubview(label1)
+//
+//        let label2 = UILabel()
+//        label2.text = "가나다라"
+//        label2.textColor = .white
+//        stackView.addArrangedSubview(label2)
+//
+//        let label3 = UILabel()
+//        label3.text = "1234"
+//        label3.textColor = .white
+//        stackView.addArrangedSubview(label3)
+//
+//        let label4 = UILabel()
+//        label4.text = "asdf"
+//        label4.textColor = .white
+//        stackView.addArrangedSubview(label4)
+//
+//        let label5 = UILabel()
+//        label5.text = "아아아아"
+//        label5.textColor = .white
+//        stackView.addArrangedSubview(label5)
+//    }
+//}
